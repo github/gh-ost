f6460215271a8e44955b74fc6a6be39f247ec7bb
Shlomi Noach <shlomi-noach@github.com>
2016-09-06 12:15:53 +0200

    added utf8mb4 test

---
+++ localtests/utf8mb4/create.sql
@@ -0,0 +1,21 @@
+drop table if exists gh_ost_test;
+create table gh_ost_test (
+  id int auto_increment,
+  t varchar(128) charset utf8mb4,
+  primary key(id)
+) auto_increment=1;
+
+drop event if exists gh_ost_test;
+delimiter ;;
+create event gh_ost_test
+  on schedule every 1 second
+  starts current_timestamp
+  ends current_timestamp + interval 60 second
+  on completion not preserve
+  enable
+  do
+begin
+  insert into gh_ost_test values (null, md5(rand()));
+  insert into gh_ost_test values (null, 'átesting');
+  insert into gh_ost_test values (null, '🍻😀');
+end ;;
