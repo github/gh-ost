0c35a811f7b90571608597837eca67c42c9290d5
Shlomi Noach <shlomi-noach@github.com>
2016-10-08 11:06:27 +0200

    setting time_zoe='+00:00' on rowcopy

--- localtests/tz-datetime-ts/create.sql
+++ localtests/tz-datetime-ts/create.sql
@@ -4,7 +4,7 @@ create table gh_ost_test (
   i int not null,
   ts0 timestamp default current_timestamp,
   ts1 timestamp,
-  ts2 timestamp,
+  ts2 datetime,
   t   datetime,
   updated tinyint unsigned default 0,
   primary key(id),
@@ -21,11 +21,13 @@ create event gh_ost_test
   enable
   do
 begin
+  insert into gh_ost_test values (null, 7, null, now(), now(), '2010-10-20 10:20:30', 0);
+
   insert into gh_ost_test values (null, 11, null, now(), now(), '2010-10-20 10:20:30', 0);
   update gh_ost_test set ts2=now() + interval 10 minute, updated = 1 where i = 11 order by id desc limit 1;
 
   set session time_zone='system';
-  insert into gh_ost_test values (null, 11, null, now(), now(), '2010-10-20 10:20:30', 0);
+  insert into gh_ost_test values (null, 13, null, now(), now(), '2010-10-20 10:20:30', 0);
   update gh_ost_test set ts2=now() + interval 10 minute, updated = 1 where i = 13 order by id desc limit 1;
 
   set session time_zone='+00:00';
