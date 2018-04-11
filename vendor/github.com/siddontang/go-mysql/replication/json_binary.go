2376c917045f88848da423423dc241ca7ea010e9
Shlomi Noach <shlomi-noach@github.com>
2018-04-11 09:11:27 +0300

    Fix to long JSON values

--- vendor/github.com/siddontang/go-mysql/replication/json_binary.go
+++ vendor/github.com/siddontang/go-mysql/replication/json_binary.go
@@ -338,7 +338,7 @@ func (d *jsonBinaryDecoder) decodeString(data []byte) string {
 
 	l, n := d.decodeVariableLength(data)
 
-	if d.isDataShort(data, int(l)+n) {
+	if d.isDataShort(data, l+n) {
 		return ""
 	}
 
@@ -358,11 +358,11 @@ func (d *jsonBinaryDecoder) decodeOpaque(data []byte) interface{} {
 
 	l, n := d.decodeVariableLength(data)
 
-	if d.isDataShort(data, int(l)+n) {
+	if d.isDataShort(data, l+n) {
 		return nil
 	}
 
-	data = data[n : int(l)+n]
+	data = data[n : l+n]
 
 	switch tp {
 	case MYSQL_TYPE_NEWDECIMAL:
@@ -459,7 +459,7 @@ func (d *jsonBinaryDecoder) decodeVariableLength(data []byte) (int, int) {
 	length := uint64(0)
 	for ; pos < maxCount; pos++ {
 		v := data[pos]
-		length = (length << 7) + uint64(v&0x7F)
+		length |= uint64(v&0x7F) << uint(7*pos)
 
 		if v&0x80 == 0 {
 			if length > math.MaxUint32 {
