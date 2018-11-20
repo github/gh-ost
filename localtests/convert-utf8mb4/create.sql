a7aa4eb2c03f58e92e845818b97c358f9d14aee1
Shlomi Noach <shlomi-noach@github.com>
2018-11-20 19:48:59 +0200

    adding latin1

--- localtests/convert-utf8mb4/create.sql
+++ localtests/convert-utf8mb4/create.sql
@@ -2,11 +2,15 @@ drop table if exists gh_ost_test;
 create table gh_ost_test (
   id int auto_increment,
   t varchar(128) charset utf8 collate utf8_general_ci,
+  tl varchar(128) charset latin1 not null,
   ta varchar(128) charset ascii not null,
   primary key(id)
 ) auto_increment=1;
 
-insert into gh_ost_test values (null, 'Hello world, Καλημέρα κόσμε, コンニチハ', 'initial');
+insert into gh_ost_test values (null, 'átesting');
+
+
+insert into gh_ost_test values (null, 'Hello world, Καλημέρα κόσμε, コンニチハ', 'átesting0', 'initial');
 
 drop event if exists gh_ost_test;
 delimiter ;;
@@ -18,10 +22,10 @@ create event gh_ost_test
   enable
   do
 begin
-  insert into gh_ost_test values (null, md5(rand()), 'a');
-  insert into gh_ost_test values (null, 'novo proprietário', 'b');
-  insert into gh_ost_test values (null, '2H₂ + O₂ ⇌ 2H₂O, R = 4.7 kΩ, ⌀ 200 mm', 'c');
-  insert into gh_ost_test values (null, 'usuário', 'x');
+  insert into gh_ost_test values (null, md5(rand()), 'átesting-a', 'a');
+  insert into gh_ost_test values (null, 'novo proprietário', 'átesting-b', 'b');
+  insert into gh_ost_test values (null, '2H₂ + O₂ ⇌ 2H₂O, R = 4.7 kΩ, ⌀ 200 mm', 'átesting-c', 'c');
+  insert into gh_ost_test values (null, 'usuário', 'átesting-x', 'x');
 
   delete from gh_ost_test where ta='x' order by id desc limit 1;
 end ;;
