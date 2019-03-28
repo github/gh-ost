85ab1bdfced7a1e2eceae520b4a584432c283846
Juan Manuel Fernández García-Minguillán <jmfernandez@tuenti.com>
2019-03-28 11:08:23 +0100

    Added changes to use a cutover based on triggers

--- go/base/context.go
+++ go/base/context.go
@@ -38,6 +38,7 @@ type CutOver int
 const (
 	CutOverAtomic CutOver = iota
 	CutOverTwoStep
+	CutOverTrigger
 )
 
 type ThrottleReasonHint string
@@ -194,6 +195,7 @@ type MigrationContext struct {
 	UserCommandedUnpostponeFlag            int64
 	CutOverCompleteFlag                    int64
 	InCutOverCriticalSectionFlag           int64
+	ApplyDMLEventState      int64
 	PanicAbort                             chan error
 
 	OriginalTableColumnsOnApplier    *sql.ColumnList
--- go/cmd/gh-ost/main.go
+++ go/cmd/gh-ost/main.go
@@ -226,6 +226,8 @@ func main() {
 		migrationContext.CutOverType = base.CutOverAtomic
 	case "two-step":
 		migrationContext.CutOverType = base.CutOverTwoStep
+	case "trigger":
+		migrationContext.CutOverType = base.CutOverTrigger
 	default:
 		log.Fatalf("Unknown cut-over: %s", *cutOver)
 	}
