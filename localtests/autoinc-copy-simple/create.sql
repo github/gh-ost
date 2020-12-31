75009db84911867a3fc37b3d2efcf442064e5753
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2020-12-31 09:50:56 +0200

    Adding simple test for 'expect_table_structure' scenario

---
+++ localtests/autoinc-copy-simple/create.sql
@@ -0,0 +1,11 @@
+drop table if exists gh_ost_test;
+create table gh_ost_test (
+  id int auto_increment,
+  i int not null,
+  primary key(id)
+) auto_increment=1;
+
+insert into gh_ost_test values (NULL, 11);
+insert into gh_ost_test values (NULL, 13);
+insert into gh_ost_test values (NULL, 17);
+insert into gh_ost_test values (NULL, 23);
