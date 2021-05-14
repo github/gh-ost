c71dbf9ef3e8511b1eefb3ec38ef6d3ce768f0e6
Tim Vaillancourt <timvaillancourt@github.com>
2021-05-14 15:32:56 +0200

    Copy auto increment (#967)
    * v1.1.0
    
    * WIP: copying AUTO_INCREMENT value to ghost table
    Initial commit: towards setting up a test suite
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    
    * greping for 'expect_table_structure' content
    
    * Adding simple test for 'expect_table_structure' scenario
    
    * adding tests for AUTO_INCREMENT value after row deletes. Should initially fail
    
    * clear event beforehand
    
    * parsing AUTO_INCREMENT from alter query, reading AUTO_INCREMENT from original table, applying AUTO_INCREMENT value onto ghost table if applicable and user has not specified AUTO_INCREMENT in alter statement
    
    * support GetUint64
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    
    * minor update to test
    
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
    
    * adding test for user defined AUTO_INCREMENT statement
    
    Co-authored-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- localtests/test.sh
+++ localtests/test.sh
@@ -12,6 +12,7 @@ test_logfile=/tmp/gh-ost-test.log
 default_ghost_binary=/tmp/gh-ost-test
 ghost_binary=""
 exec_command_file=/tmp/gh-ost-test.bash
+ghost_structure_output_file=/tmp/gh-ost-test.ghost.structure.sql
 orig_content_output_file=/tmp/gh-ost-test.orig.content.csv
 ghost_content_output_file=/tmp/gh-ost-test.ghost.content.csv
 throttle_flag_file=/tmp/gh-ost-test.ghost.throttle.flag
@@ -202,6 +203,18 @@ test_single() {
     echo "ERROR $test_name execution failure. cat $test_logfile:"
     cat $test_logfile
     return 1
+  fi
+
+  gh-ost-test-mysql-replica --default-character-set=utf8mb4 test -e "show create table _gh_ost_test_gho\G" -ss > $ghost_structure_output_file
+
+  if [ -f $tests_path/$test_name/expect_table_structure ] ; then
+    expected_table_structure="$(cat $tests_path/$test_name/expect_table_structure)"
+    if ! grep -q "$expected_table_structure" $ghost_structure_output_file ; then
+      echo
+      echo "ERROR $test_name: table structure was expected to include ${expected_table_structure} but did not. cat $ghost_structure_output_file:"
+      cat $ghost_structure_output_file
+      return 1
+    fi
   fi
 
   echo_dot
