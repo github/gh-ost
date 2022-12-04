e1ce2340c1e5fe90223c9db52975555a7ac2099e
Tim Vaillancourt <tim@timvaillancourt.com>
2022-12-04 02:04:10 +0100

    Add GITHUB_ACTION env

--- localtests/mysql-env.sh
+++ localtests/mysql-env.sh
@@ -4,8 +4,8 @@ DIR=$(readlink -f $(dirname $0))
 FILE=$DIR/mysql.env
 
 (
+  echo "GITHUB_ACTION=${GITHUB_ACTION}"
   echo 'MYSQL_ALLOW_EMPTY_PASSWORD=true'
-
   echo "TEST_STORAGE_ENGINE=${TEST_STORAGE_ENGINE}"
   if [ "$TEST_STORAGE_ENGINE" == "rocksdb" ]; then
     echo 'INIT_ROCKSDB=true'
