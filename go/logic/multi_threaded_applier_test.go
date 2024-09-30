b44c6df666e4b6b2cb44df55ffe259a3ac2cd174
Arthur Schreiber <arthurschreiber@github.com>
2024-09-30 14:43:42 +0000

    Fix deadlocks.

--- go/logic/multi_threaded_applier_test.go
+++ go/logic/multi_threaded_applier_test.go
@@ -10,25 +10,42 @@ import (
 
 type Job struct {
 	// The sequence number of the job
-	sequenceNumber int
+	sequenceNumber int64
 
 	// The sequence number of the job this job depends on
-	lastCommitted int
+	lastCommitted int64
+
+	// channel that's closed once the job's dependencies are met
+	// `nil` if the job's dependencies are already met when the job is submitted
+	waitChannel chan struct{}
 }
 
 type Coordinator struct {
-	lowWaterMark int
-
+	// The queue of jobs to be executed
 	queue chan *Job
 
 	wg sync.WaitGroup
 
-	mu            sync.Mutex
-	completedJobs map[int]bool
-	waitingJobs   map[int][]*Job
-	firstJob      bool
-
+	// List of workers
 	workers []*Worker
+
+	// Mutex to protect the fields below
+	mu sync.Mutex
+
+	// The low water mark. This is the sequence number of the last job that has been committed.
+	lowWaterMark int64
+
+	// This is a map of completed jobs by their sequence numbers.
+	// This is used when updating the low water mark.
+	completedJobs map[int64]bool
+
+	// These are the jobs that are waiting for a previous job to complete.
+	// They are indexed by the sequence number of the job they are waiting for.
+	waitingJobs map[int64][]*Job
+
+	// Is this the first job we're processing? If yes, we can schedule it even if the last committed
+	// sequence number is greater than the low water mark.
+	firstJob bool
 }
 
 type Worker struct {
@@ -39,20 +56,29 @@ func NewCoordinator() *Coordinator {
 	return &Coordinator{
 		lowWaterMark:  0,
 		firstJob:      true,
-		completedJobs: make(map[int]bool),
-		waitingJobs:   make(map[int][]*Job),
-		queue:         make(chan *Job, 100),
+		completedJobs: make(map[int64]bool),
+		waitingJobs:   make(map[int64][]*Job),
+		queue:         make(chan *Job),
 	}
 }
 
 func (c *Coordinator) StartWorkers(count int) {
 	for i := 0; i < count; i++ {
-		go func() {
-			w := Worker{}
-			c.workers = append(c.workers, &w)
+		w := Worker{}
+		c.workers = append(c.workers, &w)
 
+		go func() {
 			for job := range c.queue {
+				// fmt.Printf("Worker processing job: %d\n", job.sequenceNumber)
+
+				if job.waitChannel != nil {
+					// fmt.Printf("Worker waiting for job: %d\n", job.sequenceNumber)
+					<-job.waitChannel
+					// fmt.Printf("Worker received signal for job: %d\n", job.sequenceNumber)
+				}
+
 				w.processJob(job)
+				// fmt.Printf("Marking job as completed: %d\n", job.sequenceNumber)
 				c.markJobCompleted(job)
 			}
 		}()
@@ -61,42 +87,50 @@ func (c *Coordinator) StartWorkers(count int) {
 
 func (c *Coordinator) SubmitJob(job *Job) {
 	c.mu.Lock()
-	defer c.mu.Unlock()
 
 	c.wg.Add(1)
 
-	// If the job is ready to be scheduled, schedule it.
+	// Jobs might need for their dependencies to be met before they can be executed. We use the
+	// `waitChannel` to signal that the job's dependencies are met.
 	//
-	// A job can be scheduled if:
-	// * This is the very first job.
-	// * The last committed job is less than or equal to the low water mark. This
-	//   means that all jobs up to the low water mark have been completed.
+	// We can short-circuit this if:
+	// * This is the first job we're processing
+	// * The job's last committed sequence number is less than or equal to the low water mark,
+	//   thus we know that the job's dependencies are already met.
+	// * The job's dependency has completed, but the lowWaterMark has not been updated yet because
+	//   a job with a lower sequence number is still being processed.
+	//
+	// When short-circuiting, we don't assign a `waitChannel` to the job, thus signaling that the
+	// job's dependencies are already met.
 	if c.firstJob {
-		fmt.Printf("Scheduling first job: %d\n", job.sequenceNumber)
-		c.queue <- job
+		// fmt.Printf("Scheduling first job: %d\n", job.sequenceNumber)
 		c.firstJob = false
-	} else if job.lastCommitted <= c.lowWaterMark {
-		fmt.Printf("Scheduling job: %d\n", job.sequenceNumber)
-		c.queue <- job
+	} else if job.lastCommitted <= c.lowWaterMark || c.completedJobs[job.lastCommitted] {
+		// fmt.Printf("Scheduling job: %d\n", job.sequenceNumber)
 	} else {
-		fmt.Printf("Job %d is waiting for job %d to complete\n", job.sequenceNumber, job.lastCommitted)
+		// fmt.Printf("Job %d is waiting for job %d to complete\n", job.sequenceNumber, job.lastCommitted)
+
+		job.waitChannel = make(chan struct{})
 		c.waitingJobs[job.lastCommitted] = append(c.waitingJobs[job.lastCommitted], job)
 	}
+
+	c.mu.Unlock()
+
+	// Add the job to the queue. This will block if no worker is available to pick this job up.
+	c.queue <- job
 }
 
 func (c *Coordinator) markJobCompleted(job *Job) {
 	c.mu.Lock()
-	defer c.mu.Unlock()
+	defer c.wg.Done()
 
-	c.wg.Done()
-
-	fmt.Printf("Marking job as completed: %d\n", job.sequenceNumber)
+	// fmt.Printf("Marking job as completed: %d\n", job.sequenceNumber)
 
 	// Mark the job as completed
 	c.completedJobs[job.sequenceNumber] = true
 
 	// Then, update the low water mark if possible
-	fmt.Printf("Low water mark before update: %d\n", c.lowWaterMark)
+	// fmt.Printf("Low water mark before update: %d\n", c.lowWaterMark)
 	for {
 		if c.completedJobs[c.lowWaterMark+1] {
 			c.lowWaterMark++
@@ -105,23 +139,32 @@ func (c *Coordinator) markJobCompleted(job *Job) {
 			break
 		}
 	}
-	fmt.Printf("Low water mark after update: %d\n", c.lowWaterMark)
+	// fmt.Printf("Low water mark after update: %d\n", c.lowWaterMark)
+
+	jobsToNotify := make([]*Job, 0)
 
 	// Schedule any jobs that were waiting for this job to complete
 	for lastCommitted, jobs := range c.waitingJobs {
 		if lastCommitted <= c.lowWaterMark {
-			for _, waitingJob := range jobs {
-				fmt.Printf("Scheduling previously waiting job: %d\n", waitingJob.sequenceNumber)
-				c.queue <- waitingJob
-			}
+			jobsToNotify = append(jobsToNotify, jobs...)
 			delete(c.waitingJobs, lastCommitted)
 		}
+	}
+
+	c.mu.Unlock()
+
+	for _, waitingJob := range jobsToNotify {
+		// fmt.Printf("Scheduling previously waiting job: %d - %d\n", waitingJob.sequenceNumber, waitingJob.lastCommitted)
+		waitingJob.waitChannel <- struct{}{}
 	}
 }
 
 func (w *Worker) processJob(job *Job) error {
-	// sleep random time between 1 and 100 ms to simulate work
-	time.Sleep(time.Duration(rand.Intn(100)+1) * time.Millisecond)
+	// Simulate `BEGIN`
+	time.Sleep(100 * time.Microsecond)
+
+	// Simulate `COMMIT`
+	time.Sleep(100 * time.Microsecond)
 
 	w.executedJobs++
 
@@ -132,7 +175,7 @@ func TestMultiThreadedApplier(t *testing.T) {
 	coordinator := NewCoordinator()
 	coordinator.StartWorkers(16)
 
-	for i := 1; i < 101; i++ {
+	for i := int64(1); i < 3000; i++ {
 		coordinator.SubmitJob(&Job{sequenceNumber: i, lastCommitted: i - 1})
 	}
 
@@ -147,7 +190,7 @@ func TestMultiThreadedApplierWithDependentJobs(t *testing.T) {
 	coordinator := NewCoordinator()
 	coordinator.StartWorkers(16)
 
-	for i := 1; i < 101; i++ {
+	for i := int64(1); i < 3000; i++ {
 		coordinator.SubmitJob(&Job{sequenceNumber: i, lastCommitted: ((i - 1) / 10) * 10})
 	}
 
@@ -162,7 +205,7 @@ func TestMultiThreadedApplierWithManyDependentJobs(t *testing.T) {
 	coordinator := NewCoordinator()
 	coordinator.StartWorkers(16)
 
-	for i := 1; i < 101; i++ {
+	for i := int64(1); i < 3000; i++ {
 		coordinator.SubmitJob(&Job{sequenceNumber: i, lastCommitted: 1})
 	}
 
@@ -177,8 +220,8 @@ func TestMultiThreadedApplierWithVaryingDependentJobs(t *testing.T) {
 	coordinator := NewCoordinator()
 	coordinator.StartWorkers(16)
 
-	for i := 1; i < 101; i++ {
-		coordinator.SubmitJob(&Job{sequenceNumber: i, lastCommitted: rand.Intn(i)})
+	for i := int64(1); i < 3000; i++ {
+		coordinator.SubmitJob(&Job{sequenceNumber: i, lastCommitted: rand.Int63n(i)})
 	}
 
 	coordinator.wg.Wait()
