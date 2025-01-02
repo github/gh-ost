0fa2033c6214c24ab1354c33649b52f6714e3556
Peter Vandoros <peter.vandoros@envato.com>
2025-01-02 13:27:43 +1100

    Register TLS config when setting up the throttler's connection info
    This ensures that the throttler's TLS config has been registered with
    the mysql driver before any connections are attempted.
    
    This is the second part of resolving the following TLS verify
    error:
    
    2025-01-02 02:07:26 FATAL tls: failed to verify certificate: x509: certificate is valid for [old host], not [new host]

--- go/logic/throttler.go
+++ go/logic/throttler.go
@@ -216,6 +216,9 @@ func (this *Throttler) collectControlReplicasLag() {
 		lagResults := make(chan *mysql.ReplicationLagResult, instanceKeyMap.Len())
 		for replicaKey := range *instanceKeyMap {
 			connectionConfig := this.migrationContext.InspectorConnectionConfig.DuplicateCredentials(replicaKey)
+			if err := connectionConfig.RegisterTLSConfig(); err != nil {
+				return &mysql.ReplicationLagResult{Err: err}
+			}
 
 			lagResult := &mysql.ReplicationLagResult{Key: connectionConfig.Key}
 			go func() {
