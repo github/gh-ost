cb1cf02a9d19660e59b45f8f416bc8b143b112a5
Zach Sierakowski <zacharysierakowski@github.com>
2026-06-22 17:09:56 +0000

    move-tables: report target lag accurately
    Update status reporting and lag measurement for move-table migrations.
    
    Refs: #1721

--- go/binlog/gomysql_reader.go
+++ go/binlog/gomysql_reader.go
@@ -122,6 +122,7 @@ func (gmr *GoMySQLReader) handleRowsEvent(ev *replication.BinlogEvent, rowsEvent
 			continue
 		}
 		binlogEntry := NewBinlogEntryAt(currentCoords)
+		binlogEntry.Timestamp = time.Unix(int64(ev.Header.Timestamp), 0)
 		binlogEntry.DmlEvent = NewBinlogDMLEvent(
 			string(rowsEvent.Table.Schema),
 			string(rowsEvent.Table.Table),
